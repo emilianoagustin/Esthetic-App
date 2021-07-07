909f9558eb46f71210fe76ea9c3ba4b5a0a33e46
eugebutta <eugebutta90@gmail.com>
2021-07-07 18:53:45 -0300

    route login & home

--- client/src/components/Container/App.js
+++ client/src/components/Container/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header/Header'
+import Login from '../Login/Login'
+import { Route } from "react-router-dom";
 
 
 const App = () => {
   return (
    <React.Fragment>
-    <Header/>
+     
+   
+    <Route path={["/login", "/home"]} component={Header} />
+    <Route path="/login" exact component={Login} />
+   
    </React.Fragment>
 
   );
