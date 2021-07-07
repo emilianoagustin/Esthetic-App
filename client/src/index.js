b33813b5fd74f3c68fd21f39d800328e86a9e165
eugebutta <eugebutta90@gmail.com>
2021-07-07 13:46:51 -0300

    setting index.

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './store/store';
+import { Provider } from 'react-redux';
 import App from './components/Container/App';
+import { BrowserRouter } from 'react-router-dom'
+/* import reportWebVitals from './reportWebVitals'; */
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Provider  store={store} > 
+     <App/>
+    </Provider> 
+  </BrowserRouter> 
+</React.StrictMode>,
   document.getElementById('root')
 );
+/* reportWebVitals(); */
