935d8eba27824f9964c716f07f259a7a10e5c472
eugebutta <eugebutta90@gmail.com>
2021-07-07 13:48:00 -0300

    setting store(sin error)

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,11 +1,11 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "./Reducer";
+/* import rootReducer from "./Reducer"; */
 import thunk from "redux-thunk";
 
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 
- const store = createStore(rootReducer, composeEnhancers(
+ const store = createStore( composeEnhancers(
     applyMiddleware(thunk)
   ))
 
