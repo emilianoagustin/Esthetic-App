f06b47aeeddc7fcb7a5ecb370e217e48ebd9044f
Mario Martinez <marioaviva@gmail.com>
2021-07-07 13:34:29 -0500

    add method for recive image from users

--- api/src/models/Users.ts
+++ api/src/models/Users.ts
@@ -2,6 +2,9 @@ import { Schema, model } from 'mongoose';
 
 const UserSchema = new Schema(
   {
+    picture: {
+      type: String,
+    },
     firstName: {
       type: String,
       required: true,
@@ -36,5 +39,7 @@ const UserSchema = new Schema(
   { versionKey: false, timestamps: true }
   // versionKey para quitar el anuncio molesto de mongodb y timestamps para  saber cuando fue creado y cuando fue actualizado
 );
+
+UserSchema.methods.setPicture = () => {};
 
 export default model('Users', UserSchema); // la funcion model recibe 2 parametros el primero en nombre del modelo y el segundo el schema
