3e96757f24583299337c84947f0d99b20cd54bae
eugebutta <eugebutta90@gmail.com>
2021-07-07 19:27:22 -0300

    change index

--- client/src/index.js
+++ client/src/index.js
@@ -4,16 +4,24 @@ import store from './store/store';
 import { Provider } from 'react-redux';
 import App from './components/Container/App';
 import { BrowserRouter } from 'react-router-dom'
+import { render } from "react-dom";
 /* import reportWebVitals from './reportWebVitals'; */
 
-ReactDOM.render(
-  <React.StrictMode>
-  <BrowserRouter>
-    <Provider  store={store} > 
-     <App/>
-    </Provider> 
-  </BrowserRouter> 
-</React.StrictMode>,
-  document.getElementById('root')
-);
-/* reportWebVitals(); */
+export const UserContext = React.createContext();
+const Root = () => {
+  const [user, setUser] = React.useState({});
+  
+  return(
+<React.StrictMode>
+  <Provider store ={store}>
+    <BrowserRouter>
+    <UserContext.Provider value={{ user, setUser }} >
+    <App/>
+      </UserContext.Provider>
+    </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+)};
+
+export default render(<Root />, document.getElementById("root"));
+
