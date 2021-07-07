272077681ecad5a30d12925b0c83a89455ffabf7
Leandro Cadena <leo_etiec@hotmail.com>
2021-07-07 15:17:31 -0300

    add(model services)

---
+++ api/src/models/Services.ts
@@ -0,0 +1,30 @@
+import { Schema, model } from 'mongoose';
+import { SERVICES_TYPES } from '../utils/constants';
+
+const ServiceSchema = new Schema(
+    {
+        id: {
+            type: Number,
+            index: true,
+            unique: true,
+            required: true
+        },
+        name: {
+            type: String,
+            enum: SERVICES_TYPES,
+            trim: true,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        description: {
+            type: String
+        },
+
+    },
+    { versionKey: false, timestamps: true }
+);
+
+export default model('Services', ServiceSchema);
