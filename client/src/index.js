c532b45715e868ff486d6a4a7986b5c3b9e61cad
Leandro Cadena <leo_etiec@hotmail.com>
2021-07-08 11:46:15 -0300

    fixed

---
+++ client/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import store from './store/store';
+import { Provider } from 'react-redux';
+import App from './Container/App';
+import { BrowserRouter } from 'react-router-dom'
+import { render } from "react-dom";
+/* import reportWebVitals from './reportWebVitals'; */
+
+export const UserContext = React.createContext();
+
+const Root = () => {
+  const [user, setUser] = React.useState({});
+  
+  return(
+<React.StrictMode>
+  <Provider store ={store}>
+    <BrowserRouter>
+    <UserContext.Provider value={{ user, setUser }} >
+    <App/>
+      </UserContext.Provider>
+    </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+)};
+
+export default render(<Root />, document.getElementById("root"));
+
