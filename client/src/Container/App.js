f8fde6246f24db9fd60165a9f675f65d242b01f5
emilianoagustin <pintos.emiliano@gmail.com>
2021-07-09 14:15:19 -0300

    added cart route

--- client/src/Container/App.js
+++ client/src/Container/App.js
@@ -1,18 +1,20 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Login from "../components/Login/Login";
 import Home from "../components/Home/Home";
 import UserRegister from "../components/UserRegister/UserRegister";
-import { Route } from "react-router-dom";
 import Section from "../components/Section/Section.js";
+import Cart from "../components/Cart/Cart";
 
 const App = () => {
   return (
     <React.Fragment>
-      <Route path={["/login", "/home", "/userRegister"]} component={Header} />
+      <Route path={["/login", "/home", "/userRegister", "/cart"]} component={Header} />
       <Route path={["/home"]} component={(Home, Section)} />
       <Route path="/login" exact component={Login} />
       <Route path="/userRegister" exact component={UserRegister} />
+      <Route path="/cart" exact component={Cart} />
     </React.Fragment>
   );
 };
