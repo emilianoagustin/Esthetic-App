e85e9bd0719282a164072e14fc7c97e78f6e52e4
Mario Martinez <marioaviva@gmail.com>
2021-07-09 14:34:45 -0500

    add img provider with get, post, put and delete tested

--- api/src/routes/providers.routes.ts
+++ api/src/routes/providers.routes.ts
@@ -1,5 +1,5 @@
-import { Router } from "express";
-import { CreateCalendar } from "../controllers/calendar";
+import { Router } from 'express';
+import { CreateCalendar } from '../controllers/calendar';
 import {
   getAllProviders,
   // getProviderByName,
@@ -7,18 +7,20 @@ import {
   createProvider,
   deleteProvider,
   updateProvider,
-} from "../controllers/providers";
+} from '../controllers/providers';
+import upload from '../libs/multer';
+
 const router = Router();
 
-router.get("/", getAllProviders);
+router.get('/', getAllProviders);
 // router.get("/?name", getProviderByName);
-router.get("/:id", getProviderById);
-router.post("/", createProvider);
-router.delete("/:id", deleteProvider);
-router.put("/:id", updateProvider);
+router.get('/:id', getProviderById);
+router.post('/', upload.single('image'), createProvider);
+router.delete('/:id', deleteProvider);
+router.put('/:id', updateProvider);
 
 //Calendar Routes
 
-router.post("/calendar", CreateCalendar);
+router.post('/calendar', CreateCalendar);
 
 export default router;
