fd1b474f112e9e355d6b028dbe3f25b247f9ad9b
eugebutta <eugebutta90@gmail.com>
2021-07-10 11:23:49 -0300

    header, menu profile

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -13,7 +13,11 @@ import Button from "@material-ui/core/Button";
 import loto from "../../img/loto.png";
 import { BiShoppingBag } from "react-icons/bi";
 import { useSelector } from "react-redux";
-
+import Menu from "@material-ui/core/Menu";
+import Avatar from "@material-ui/core/Avatar";
+import Fade from "@material-ui/core/Fade";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import Tooltip from '@material-ui/core/Tooltip';
 
 const useStyles = makeStyles((theme) => ({
   grow: {
@@ -77,19 +81,57 @@ const useStyles = makeStyles((theme) => ({
       display: "none",
     },
   },
+  root: {
+    display: "flex",
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
 }));
+
+const options = [
+  "Zona Norte ",
+  "Zona Sur",
+  "Centro",
+  "Y. Buena Norte ",
+  "Y. Buena Sur",
+  "El Corte",
+  "La Banda",
+];
+const ITEM_HEIGHT = 48;
 
 export default function PrimarySearchAppBar() {
   const classes = useStyles();
-  const loggin = useSelector((state) => state.userActive);
-  console.log(loggin);
+  const logginState = useSelector((state) => state.userActive);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [zona, setzona] = React.useState(null);
 
+  const open = Boolean(anchorEl);
+  const abrir = Boolean(zona);
+  console.log(logginState);
 
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  
+  const handleClickZona = (event) => {
+    setzona(event.currentTarget);
+  };
  
-  const logeado = [
+
+  const handleCloseZona = () => {
+    setzona(null);
+  };
+
+  const loginAndRegister = [
     <Link to={"/login"} style={{ color: "white", textDecoration: "none" }}>
       <Button color="inherit">INGRESAR</Button>
-    </Link>, "|" , 
+    </Link>,
+    "|",
     <Link
       to={"/userRegister"}
       style={{ color: "white", textDecoration: "none" }}
@@ -98,16 +140,65 @@ export default function PrimarySearchAppBar() {
     </Link>,
   ];
 
+  const loginProfile = [
+    <Avatar
+      onClick={handleClick}
+      alt="Remy Sharp"
+      src="/static/images/avatar/1.jpg"
+    />,
+    <Menu
+      id="fade-menu"
+      anchorEl={anchorEl}
+      keepMounted
+      open={open}
+      onClose={handleClose}
+      TransitionComponent={Fade}
+    >
+      <Link to={"/perfil"} style={{ color: "black", textDecoration: "none" }}>
+        <MenuItem onClick={handleClose}>Perfil</MenuItem>
+      </Link>
+      <Link
+        to={"/perfil/historial"}
+        style={{ color: "black", textDecoration: "none" }}
+      >
+        <MenuItem onClick={handleClose}>Historial De Compras</MenuItem>
+      </Link>
+      <MenuItem onClick={handleClose}>Cerrar Sesión</MenuItem>
+    </Menu>,
+  ];
+
+  const zonas = [ <Button
+    onClick={handleClickZona}
+    color="inherit"
+    style={{ color: "white", textDecoration: "none" }}
+  >
+    ZONAS{" "}
+  </Button>,
+      <Menu
+        id="long-menu"
+        anchorEl={zona}
+        keepMounted
+        open={abrir}
+        onClose={handleCloseZona}
+        PaperProps={{
+          style: {
+            maxHeight: ITEM_HEIGHT * 4.5,
+            width: '20ch',
+          },
+        }}
+      >
+        {options.map((option) => (
+          <MenuItem key={option} selected={option === 'Pyxis'} onClick={handleCloseZona}>
+            {option}
+          </MenuItem>
+        ))}
+      </Menu>
+     ]
+
   return (
     <div className={classes.grow}>
-      <AppBar position="static" style={{ backgroundColor: "#c0b3c2" }}>
+      <AppBar position="static" style={{ backgroundColor: "#14224F" }}>
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-          ></IconButton>
           <Typography className={classes.title} variant="h6" noWrap>
             <Link to={"/home"} style={{ textDecoration: "none" }}>
               <img
@@ -134,45 +225,21 @@ export default function PrimarySearchAppBar() {
           <div className={classes.sectionDesktop}></div>
           <div style={{ display: "flex", marginRight: "2rem" }}>
             <div>
-
-            <Link
-              to={"/zonas"}
-              style={{ color: "white", textDecoration: "none" }}
+             
+              {zonas}
+              |
+              <Link
+                to={"/provaiderRegister"}
+                style={{ color: "white", textDecoration: "none" }}
               >
-              <Button color="inherit">ZONAS </Button>
-            </Link>
-            |
-            <Link
-              to={"/provaiderRegister"}
-              style={{ color: "white", textDecoration: "none" }}
-              >
-              <Button color="inherit">INSCRIBITE COMO PROFESIONAL</Button>
-            </Link>
-          </div>
-
-              </div>
-          <b>
-            {!loggin ? (
-              logeado
-            ) : (
-              <Link to= '/Perfil' style={{ color: "white", textDecoration: "none" }}>
-              <MenuItem >
-                <IconButton
-                  aria-label="account of current user"
-                  aria-controls="primary-search-account-menu"
-                  aria-haspopup="true"
-                  color="inherit"
-                >
-                  <AccountCircle />
-                </IconButton>
-                <p>Perfil</p>
-              </MenuItem>
+                <Button color="inherit">INSCRIBITE COMO PROFESIONAL</Button>
               </Link>
-              
-            )}
-          </b>
+            </div>
+          </div>
+          <b>{!logginState ? loginAndRegister : loginProfile}</b>
 
           <Link
+         
             to={"/cart"}
             style={{
               color: "white",
@@ -180,7 +247,6 @@ export default function PrimarySearchAppBar() {
               backgroundColor: "gray",
               borderRadius: 50,
               marginLeft: "1rem",
-              
             }}
           >
             <Button color="inherit">
