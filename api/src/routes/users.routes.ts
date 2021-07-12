14aa83439825a7bc0bd4ae2c32fc4ce98c42b87f
Dabuaf86 <dabuaf@gmail.com>
2021-07-12 10:40:00 -0300

    Fixed model users & creditCards

--- api/src/routes/users.routes.ts
+++ api/src/routes/users.routes.ts
@@ -1,12 +1,11 @@
-import { Router } from 'express';
-import * as usersCtrl from '../controllers/getUsers';
+import { Router } from "express";
+import * as usersCtrl from "../controllers/getUsers";
 import {
   getAllAddresses,
   getOneAddress,
   createAddress,
   updateAddress,
   deleteAddress,
-<<<<<<< HEAD
 } from "../controllers/addresses";
 import {
   getAllCreditCards,
@@ -15,16 +14,12 @@ import {
   updateCreditCard,
   deleteCreditCard,
 } from "../controllers/creditCards";
-=======
-} from '../controllers/addresses';
->>>>>>> f8dd1650200ccd6989f7c122ca9f6eac19b12109
 
 var multer = require("multer");
 var upload = multer({ dest: "uploads/" });
 
 const router = Router();
 
-<<<<<<< HEAD
 // <<PLAIN USERS ROUTES>>
 router.get("/", usersCtrl.getUsers);
 router.get("/:id", usersCtrl.getUser);
@@ -32,22 +27,15 @@ router.post("/", upload.single("image"), usersCtrl.createUser);
 // router.post('/', usersCtrl.createUser);
 router.delete("/:id", usersCtrl.deleteUser);
 router.put("/:id", usersCtrl.updateUser);
-
-=======
-router.get('/', usersCtrl.getUsers);
-router.get('/:id', usersCtrl.getUser);
-router.delete('/:id', usersCtrl.deleteUser);
-router.put('/:id', usersCtrl.updateUser);
->>>>>>> f8dd1650200ccd6989f7c122ca9f6eac19b12109
 // router.put('/assignService/:id', usersCtrl.assignService);
 // router.put('/removeService/:id', usersCtrl.removeService);
 
 // <<Routes to users' addresses>>
-router.get('/addresses', getAllAddresses);
-router.get('/addresses/:id', getOneAddress);
-router.post('/addresses', createAddress);
-router.put('/addresses/:id', updateAddress);
-router.delete('/addresses/:id', deleteAddress);
+router.get("/addresses", getAllAddresses);
+router.get("/addresses/:id", getOneAddress);
+router.post("/addresses", createAddress);
+router.put("/addresses/:id", updateAddress);
+router.delete("/addresses/:id", deleteAddress);
 
 // <<Routes to creditCards>>
 router.get("/creditCards", getAllCreditCards);
