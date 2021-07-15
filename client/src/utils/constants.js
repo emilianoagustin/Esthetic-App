1c65656af81383f28f464e2b236a1d87667a0d8b
emilianoagustin <pintos.emiliano@gmail.com>
2021-07-15 14:36:54 -0300

    modified these files to bypass conflicts at merging

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,6 +1,5 @@
 export const getServicesBack = "http://localhost:3002/services"
 export const getProvidersByService = "http://localhost:3002/providers/services"
-export const URL_GET_PROVIDERS = "http://localhost:3002/providers/"
 
 
 export const HOST = "http://localhost:3002";
