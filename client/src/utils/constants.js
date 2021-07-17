b6a57fdb15761dae37de1c44f8e65d3db617b268
emilianoagustin <pintos.emiliano@gmail.com>
2021-07-17 17:31:05 -0300

    created PUT_PROVIDERS_ADDRESS const

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -2,6 +2,7 @@ export const HOST = "http://localhost:3002";
 export const GET_USERS = "http://localhost:3002/users";
 export const GET_PROVIDERS = "http://localhost:3002/providers";
 export const GET_SERVICES = "http://localhost:3002/services";
+export const PUT_PROVIDERS_ADDRESS = "http://localhost:3002/providers/addresses/";
 
 
 export const ID = 2
