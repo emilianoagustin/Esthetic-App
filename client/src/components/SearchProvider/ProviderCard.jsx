5db92339dea48f24ffac55e7ce3a2cc8628737d9
eugebutta <eugebutta90@gmail.com>
2021-07-20 17:36:26 -0300

    add Show providers and Search Bar

---
+++ client/src/components/SearchProvider/ProviderCard.jsx
@@ -0,0 +1,174 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+//Material UI
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red, green } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import WhatsAppIcon from '@material-ui/icons/WhatsApp';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
+import '../HomeProviders/PendingServices/PendingServices.scss';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+    boxShadow: '4px 4px 30px #e1b2f0',
+    borderColor: 'grey',
+    filter: 'saturate(80%)',
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+  favorite: {
+    color: red[500],
+  },
+  whatsApp: {
+    color: green[500],
+  },
+  font: {
+    fontSize: '18px',
+  },
+}));
+
+export default function RecipeReviewCard({ data }) {
+  const whatsApp = 'https://web.whatsapp.com/';
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+  const [stateFav, setStateFav] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+  const handleFavorites = () => {
+    setStateFav(!stateFav);
+  };
+
+  return (
+    <div className='card-services'>
+      <Card className={classes.root}>
+        <Typography variant='h3'></Typography>
+
+        <CardHeader
+          avatar={
+            <Avatar
+              aria-label='recipe'
+              className={classes.avatar}
+              src={data.image}
+            ></Avatar>
+          }
+          action={
+            <IconButton aria-label='settings'>
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title={data.name ? data.name : `${data.firstName} ${data.lastName}`}
+          subheader={data.price ? `$ ${data.price}` : ''}
+        />
+        <Link to={`/profile`}>
+          <CardMedia
+            className={classes.media}
+            image={data.image}
+            title='Paella dish'
+          />
+        </Link>
+        <CardContent>
+          <Typography variant='body2' color='textSecondary' component='p'>
+            {data.description && data.description.slice(0, 86).concat('...')}
+            {data.services &&
+              'Los servicios adquiridos y los datos principales del cliente se encuentran al desplegando la flecha...'}
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton
+            aria-label='add to favorites'
+            onClick={handleFavorites}
+            className={stateFav ? classes.favorite : ''}
+          >
+            <FavoriteIcon />
+          </IconButton>
+
+          <IconButton
+            aria-label='share'
+            className={classes.whatsApp}
+            href={whatsApp}
+          >
+            <WhatsAppIcon />
+          </IconButton>
+
+          {data.addresses && (
+            <IconButton aria-label='location'>
+              <LocationOnIcon />
+            </IconButton>
+          )}
+
+          <IconButton
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded,
+            })}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label='show more'
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={expanded} timeout='auto' unmountOnExit>
+          <CardContent>
+            {data.description && (
+              <>
+                <Typography paragraph>Descripción:</Typography>
+                <Typography paragraph>{data.description}</Typography>
+              </>
+            )}
+            {data.services && (
+              <>
+                <Typography paragraph>Servicio(s):</Typography>
+                <Typography paragraph>
+                  {data.services.map((service) => (
+                    <li key={service}>{service}</li>
+                  ))}
+                </Typography>
+
+                <Typography paragraph>Correo:</Typography>
+                <Typography paragraph>{data.email}</Typography>
+
+                <Typography paragraph>Telefono:</Typography>
+                <Typography paragraph>{data.phone}</Typography>
+
+                <Typography paragraph>Genero:</Typography>
+                <Typography paragraph>{data.gender}</Typography>
+              </>
+            )}
+          </CardContent>
+        </Collapse>
+      </Card>
+    </div>
+  );
+}
