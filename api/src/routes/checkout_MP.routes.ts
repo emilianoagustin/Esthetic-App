c678971797526cd0fa5f713e16897b3776557991
Dabuaf86 <dabuaf@gmail.com>
2021-07-20 01:44:28 -0300

    added feat mp route/ctrlr/component/modif html

---
+++ api/src/routes/checkout_MP.routes.ts
@@ -0,0 +1,8 @@
+// import { Router } from "express";
+// import { postPayment } from "../controllers/checkout_MP";
+
+// const router = Router();
+
+// router.post("/", postPayment);
+
+// export default router;
