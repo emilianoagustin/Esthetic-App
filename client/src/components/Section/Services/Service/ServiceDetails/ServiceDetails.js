b25622d9e6663564627b2bec7ae2a5d32ab00dce
Mario Martinez <marioaviva@gmail.com>
2021-07-19 21:46:44 -0500

    HomeProvider(add container hours, delet hours and Section component factorize code

--- client/src/components/Section/Services/Service/ServiceDetails/ServiceDetails.js
+++ client/src/components/Section/Services/Service/ServiceDetails/ServiceDetails.js
@@ -1,42 +1,42 @@
-import React, { useEffect } from "react";
-import "./ServiceDetails.css";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import { getServiceDetails } from "../../../../../Redux/actions/actions";
-import defaultImg from "../../../../../img/wall-cart.jpg";
+// import React, { useEffect } from "react";
+// import "./ServiceDetails.css";
+// import { useDispatch, useSelector } from "react-redux";
+// import { useParams } from "react-router-dom";
+// import { getServiceDetails } from "../../../../../Redux/actions/actions";
+// import defaultImg from "../../../../../img/wall-cart.jpg";
 
-function ServiceDetails() {
-  const dispatch = useDispatch();
-  const service = useSelector((state) => state.serviceDetails.data);
+// function ServiceDetails() {
+//   const dispatch = useDispatch();
+//   const service = useSelector((state) => state.serviceDetails.data);
 
-  var data = {};
+//   var data = {};
 
-  if (service) {
-    data = service;
-  }
-  const { id } = useParams();
+//   if (service) {
+//     data = service;
+//   }
+//   const { id } = useParams();
 
-  useEffect(() => {
-    dispatch(getServiceDetails(id));
-    return () => {};
-  }, []);
+//   useEffect(() => {
+//     dispatch(getServiceDetails(id));
+//     return () => {};
+//   }, []);
 
-  return (
-    <div className="service-container">
-      <div className="service-details">
-        {data.img ? (
-          <img className="img" src={data.img} alt="Service Image"></img>
-        ) : (
-          <img className="img" src={defaultImg} alt="Default Image"></img>
-        )}
-        <div className="service-data">
-          <h1>{service && service.name}</h1><hr />
-          <p>{service && service.description}</p>
-          <p>AR$ {service && service.price}</p>
-        </div>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="service-container">
+//       <div className="service-details">
+//         {data.img ? (
+//           <img className="img" src={data.img} alt="Service Image"></img>
+//         ) : (
+//           <img className="img" src={defaultImg} alt="Default Image"></img>
+//         )}
+//         <div className="service-data">
+//           <h1>{service && service.name}</h1><hr />
+//           <p>{service && service.description}</p>
+//           <p>AR$ {service && service.price}</p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default ServiceDetails;
+// export default ServiceDetails;
