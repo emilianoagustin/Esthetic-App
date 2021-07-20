cfadd4486ea63df32afc2cf254048684cadc660f
Dabuaf86 <dabuaf@gmail.com>
2021-07-20 10:09:29 -0300

    last version  merged

--- client/src/components/Section/Services/Services.js
+++ client/src/components/Section/Services/Services.js
@@ -16,13 +16,6 @@ export function Services({ services }) {
 
   return (
     <div>
-<<<<<<< HEAD
-      <h1 className="title">SERVICIOS</h1>
-      <div className="services-container">
-        {services.data &&
-          services.data.map((service, index) => (
-            <Service key={index} data={service} />
-=======
       <h1 className='title'>SERVICIOS</h1>
       <div className='services-container'>
         {services.data &&
@@ -31,7 +24,6 @@ export function Services({ services }) {
               {/* <Service key={index} data={service} /> */}
               <RecipeReviewCard data={service} key={index} />
             </>
->>>>>>> 0172c01d8a40dcffed8f1e6ed7ec530f1b444aa7
           ))}
       </div>
     </div>
